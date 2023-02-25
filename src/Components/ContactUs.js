eab2e7a02774b7147c83b87018df04dff83dadeb
MAYANK816 <mayankkhatri024@gmail.com>
2023-02-25 17:11:35 +0530

    contact us

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Lottie from "lottie-react";
+import aboutusanimation from ".././aboutusanimation.json";
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+      <div className="about-us-content">
+    <Lottie className='lottieAnimationabout' animationData={aboutusanimation} loop={true} />
+        <div className='about-us-children'>
+        <h1>Contact Us</h1>
+        <p>Thank you for visiting our website! We appreciate your interest in our products/services and welcome any questions or comments you may have. There are several ways you can get in touch with us:</p>
+        <p>Email us directly, please send your message to "hellodrunkengeeks@gmail.com". We typically respond within 24-48 hours.</p>
+      </div>
+    </div>
+    </div>
   )
 }
 
